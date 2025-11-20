7a49c61ec2ced91f8bed3f5230ce1643d99bfa13
danOdo <danielodobasic@hotmail.de>
2025-11-20 17:10:54 +0100

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 // src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
@@ -12,7 +12,7 @@ import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
 
     // Firebase App mit deinem Projekt initialisieren
     provideFirebaseApp(() => initializeApp(environment.firebase)),
